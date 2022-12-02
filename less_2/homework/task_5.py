1c3d8fb475ff7c3b31ddb741989be7869cf5af15
Тимур Бадалбаев <timurbadalbaev19@gmail.com>
2022-12-02 18:30:02 +0300

    add task_6

--- less_2/homework/task_5.py
+++ less_2/homework/task_5.py
@@ -11,17 +11,31 @@
 my_list = [7, 5, 3, 3, 2].
 """
 
-str_arr = input("Введите значения через пробел: ").split()
-num_arr = []
-for item in str_arr:
-    num_arr.append(int(item))
-print((sorted(num_arr, reverse=True)))
+# str_arr = input("Введите значения через пробел: ").split()
+# num_arr = []
+# for item in str_arr:
+#     num_arr.append(int(item))
+# print((sorted(num_arr, reverse=True)))
 
-num_arr.append(int(input("Добавьте число: ")))
-print((sorted(num_arr, reverse=True)))
+# num_arr.append(int(input("Добавьте число: ")))
+# print((sorted(num_arr, reverse=True)))
 
-num_arr.append(int(input("Добавьте число: ")))
-print((sorted(num_arr, reverse=True)))
+# num_arr.append(int(input("Добавьте число: ")))
+# print((sorted(num_arr, reverse=True)))
 
-num_arr.append(int(input("Добавьте число: ")))
-print((sorted(num_arr, reverse=True)))
+# num_arr.append(int(input("Добавьте число: ")))
+# print((sorted(num_arr, reverse=True)))
+
+my_list = [7, 5, 3, 3, 2]
+while True:
+    n = input("Введите оценку от 1 до 10 или q для выхода: ")
+    if n != 'q':
+        my_list.append(int(n))
+        my_list.sort(reverse=True)
+        print(my_list)
+    else:
+        break
+
+my_list = sorted([7, 5, 3, 3, 2, int(input("Введите новый элемент рейтинга: "))], reverse=True)
+
+print(my_list)
