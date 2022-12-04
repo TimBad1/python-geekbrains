86a5741c6efa56096220440b82567f77a611681b
Тимур Бадалбаев <timurbadalbaev19@gmail.com>
2022-12-04 14:43:25 +0300

    feat: add tasks 6,7

--- less_4/homework/task_6.py
+++ less_4/homework/task_6.py
@@ -11,3 +11,17 @@
 при котором повторение элементов списка будет прекращено.
 """
 
+from itertools import count, cycle
+
+for el in count(3):
+    if el > 10:
+        break
+    else:
+        print(el)
+
+c = 0
+for el in cycle("ABC"):
+    if c > 10:
+        break
+    print(el)
+    c += 1
