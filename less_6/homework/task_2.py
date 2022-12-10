58aa3f4d63c894182dd0c95ae1ef5aa9a776e487
Тимур Бадалбаев <timurbadalbaev19@gmail.com>
2022-12-10 22:52:22 +0300

    feat: add tasks 1,2

--- less_6/homework/task_2.py
+++ less_6/homework/task_2.py
@@ -5,17 +5,49 @@
 с готовым массивом [1,2,3,4,5,6,7,8,9,10] работает в 2 раза дольше чем [i for i in range(1, 11)]
 """
 
-from timeit import timeit
-from factorial import factorial_rec, factorial_cycle, factorial_math, factorial_lc
+from functools import reduce
+import math
+from memory_profiler import profile
 
-print(timeit("factorial_rec(100)", setup="from __main__ import factorial_rec", number=100000))
-print(timeit("factorial_cycle(100)", setup="from __main__ import factorial_cycle", number=100000))
-print(timeit("factorial_math(100)", setup="from __main__ import factorial_math", number=100000))
-print(timeit("factorial_lc(100)", setup="from __main__ import factorial_lc", number=100000))
+@profile
+def factorial_rec(n):
+    """
+    Подсчёт факториала через рекурсию
+    """
+    if n == 1:
+        return 1
+    return n * factorial_rec(n - 1)
 
-"""
-4.791663457
-2.4407517799999994
-0.5167011119999998
-4.701772784
-"""
+# print(factorial_rec(5))
+
+@profile
+def factorial_cycle(n):
+    """
+    Подсчёт факториала циклом
+    """
+    p = 1
+    for i in range(1, n + 1):
+        p *= i
+    return p
+
+# print(factorial_cycle(5))
+
+@profile
+def factorial_math(n):
+    """
+    Подсчёт факториала математическим модулем
+    """
+    return math.factorial(n)
+
+@profile
+def factorial_lc(n):
+    """
+    Подсчёт факториала методом reduce
+    """
+    return reduce(lambda x, y: x * y, [i for i in range(1, n + 1)])
+
+
+factorial_rec(10)
+factorial_cycle(10)
+factorial_math(10)
+factorial_lc(10)
