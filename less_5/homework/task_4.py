0f3bc85022969674c20da0f0c63caeb6637223d8
Тимур Бадалбаев <timurbadalbaev19@gmail.com>
2022-12-10 03:20:42 +0300

    feat: add tasks 4, 5

--- less_5/homework/task_4.py
+++ less_5/homework/task_4.py
@@ -17,17 +17,28 @@ my_dict = {
     'Four': 'Четыре',
 }
 
+with open('file_4.txt', "r", encoding="utf-8") as read_file:
+    with open('file_4_new.txt', "w", encoding="utf-8") as write_file:
+        content = read_file.readlines()
+        new_content = []
 
-file = open('file_4.txt', "r", encoding='utf-8')
-new_file = open('file_4_new.txt', "a", encoding='utf-8')
-content = file.readlines()
-# new_content = ''
-for line in content:
-    # new_content(str([my_dict[el] if el in my_dict else el for el in line]))
-
-# new_content = [my_dict[el] if el in my_dict else el for el in line]
+        for line in content:
+            new_content.append(' '.join(
+                [my_dict[item] if item in my_dict else item for item in line.split()]
+                ) + '\n')
+        print(new_content)
+        write_file.writelines(new_content)
 
 
-new_file.writelines(new_content)
-file.close()
-new_file.close()
+# # new_content = [for line in content my_dict[item] if item in my_dict else item for item in line.split()]
+# # for line in content:
+#     # line = ''.join(str([my_dict[item] if item in my_dict else item for item in line.split()]))
+#     # for el in line:
+#     #     if el in my_dict:
+#     #         value = my_dict.get(el)
+#     #         el = value
+#     # line = ' '.join(line)
+#     # print(content)
+# new_file.writelines(content)
+# file.close()
+# new_file.close()
